d8f84af996f25d133fa21bdee9f31b9288deb720
Adam Davis <mail@adamdavis.co.uk>
2020-11-15 22:57:01 +0000

    ip change

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
-# Import socket module 
-import socket 
+# Import socket module
+import socket
 from pynput.keyboard import Listener, Key
 
 s = None
@@ -7,49 +7,49 @@ s = None
 def on_press(key):
     if key == Key.left:  # If space was pressed, write a space
         print('left key was pressed')
-        s.send("left".encode('ascii')) 
+        s.send("left".encode('ascii'))
     elif key == Key.right:  # If enter was pressed, write a new line
         print('right key was pressed')
-        s.send("right".encode('ascii')) 
+        s.send("right".encode('ascii'))
     elif key == Key.enter:  # If enter was pressed, write a new line
         print('closing connection')
         s.close()
         exit()
 
 
-# local host IP '127.0.0.1' 
-host = '127.0.0.1'
+# local host IP '127.0.0.1'
+host = '192.168.1.13'
 
-# Define the port on which you want to connect 
+# Define the port on which you want to connect
 port = 12345
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-# connect to server on local computer 
-s.connect((host,port)) 
+# connect to server on local computer
+s.connect((host,port))
 
 with Listener(on_press=on_press) as listener:  # Setup the listener
 	listener.join()  # Join the thread to the main thread
 
 
 
-	# while True: 
+	# while True:
 
-	# 	# message sent to server 
-	# 	s.send(message.encode('ascii')) 
+	# 	# message sent to server
+	# 	s.send(message.encode('ascii'))
 
-	# 	# messaga received from server 
-	# 	data = s.recv(1024) 
+	# 	# messaga received from server
+	# 	data = s.recv(1024)
 
-	# 	# print the received message 
-	# 	# here it would be a reverse of sent message 
-	# 	print('Received from the server :',str(data.decode('ascii'))) 
+	# 	# print the received message
+	# 	# here it would be a reverse of sent message
+	# 	print('Received from the server :',str(data.decode('ascii')))
 
-	# 	# ask the client whether he wants to continue 
-	# 	ans = input('\nDo you want to continue(y/n) :') 
-	# 	if ans == 'y': 
+	# 	# ask the client whether he wants to continue
+	# 	ans = input('\nDo you want to continue(y/n) :')
+	# 	if ans == 'y':
 	# 		continue
-	# 	else: 
+	# 	else:
 	# 		break
-	# # close the connection 
-	# s.close() 
+	# # close the connection
+	# s.close()
